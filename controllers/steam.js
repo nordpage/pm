0afb3d8c9a5dc0605d5c35a750dacb05851c29d9
aurilaus <aurilaus@gmail.com>
2023-12-24 20:11:15 +0200

    initial

--- controllers/steam.js
+++ controllers/steam.js
@@ -2,23 +2,17 @@ const fetch = require("cross-fetch");
 const { response } = require("express");
 const AIC_URL = "https://api.steampowered.com/";
 
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI('4F3ABF72FB11931A802C9BE3571FDE6C');
+
 const getUser = async (req, res = response) => {
     const { keyword } = req.params;
 
     try {
-        const resp = await fetch(
-            `${AIC_URL}ISteamUser/GetPlayerSummaries/v0002/?key=4F3ABF72FB11931A802C9BE3571FDE6C&steamids=${keyword}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
+      const data =  steam.getUserSummary(keyword).then(summary => {
+            return summary;
 
-        const { data } = await resp.json();
-        console.log(data)
-
+        });
         res.json(data);
     } catch (err) {
         console.error(err);
